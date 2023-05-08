1be28a78bcb969a286d060deae33fc7daf6ebca4
fabio <fabioarraisgomes@hotmail.com>
2023-05-08 18:02:50 -0300

    testes com geração de CME

---
+++ TesteCME/Program.cs
@@ -0,0 +1,92 @@
+﻿// Create and configure FastNoise object
+using Microsoft.VisualBasic;
+using System.Text;
+
+DateTime start = DateTime.Now;
+
+FastNoiseLite noise = new();
+noise.SetNoiseType(FastNoiseLite.NoiseType.OpenSimplex2S);
+
+int imgSize = 856;
+
+// Gather noise data
+Float[] noiseData = new Float[imgSize * imgSize];
+int index = 0;
+
+Float minValue = new(999);
+float maxValue = 0;
+
+Random random = new();
+noise.SetSeed(random.Next());
+noise.SetNoiseType(FastNoiseLite.NoiseType.OpenSimplex2S);
+noise.SetFractalType(FastNoiseLite.FractalType.FBm);
+noise.SetFrequency(0.005f);
+noise.SetFractalOctaves(2);
+noise.SetFractalWeightedStrength(1f);
+
+int h = 300;    // x do centro da elipse
+int k = 400;    // y do centro da elipse
+int rx = 400;       // raio maior da elipse
+int ry = 150;       // raio menor da elipse
+double degree = 60; // angulo de inclinação da elipse em graus
+double radian = degree * Math.PI / 180; // conversao para radianos
+
+
+for (int y = 0; y < imgSize; y++)
+{
+    for (int x = 0; x < imgSize; x++)
+    {
+        Float valor = minValue;
+        //double distance = Math.Pow(x - h, 2) / Math.Pow(rx, 2) + Math.Pow(y - k, 2) / Math.Pow(ry, 2); // versão para elipse sem rotação
+        double cosa = Math.Cos(radian);
+        double sina = Math.Sin(radian);
+        double dd = rx * rx;
+        double DD = ry * ry;
+        double a = Math.Pow(cosa*(x-h)+sina*(y-k),2);
+        double b = Math.Pow(sina*(x-h)-cosa*(y-k),2);
+        double distance = (a/dd)+(b/DD);
+
+        if (distance <= 1)
+        {
+            valor = new Float((float)((((noise.GetNoise(x, y) + 1) / 2) * 280) /** (1 - distance)*/));
+
+            if (valor.value < minValue.value)
+                minValue.value = valor.value;
+            else if (valor.value > maxValue)
+                maxValue = valor.value;
+        }
+
+        noiseData[index] = valor;//((valor + 1) / 2) * 245;
+
+        index++;
+    }
+}
+
+File.WriteAllText("Teste.txt", "");
+StringBuilder sb = new();
+foreach (Float f in noiseData)
+{
+    sb.Append(f.value.ToString().Replace(',', '.') + ",");
+}
+sb.Remove(sb.Length - 1, 1);
+File.WriteAllText("Teste.txt", sb.ToString());
+TimeSpan time = DateTime.Now - start;
+Console.WriteLine(time.TotalSeconds);
+
+string strCmdText;
+strCmdText = "/C python generateCME.py";
+System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+
+class Float
+{
+    public Float(float value)
+    {
+        this.value = value;
+    }
+    public float value = 999f;
+
+    public override string ToString()
+    {
+        return value.ToString() + "F";
+    }
+}
---
+++ TesteCME/bin/Debug/net7.0/generateCME.py
@@ -0,0 +1,38 @@
+import os
+print(os.getcwd())
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import gridspec
+
+Nx = 856
+maps = "gist_heat"
+color = '.9'
+
+f = open("teste.txt", "r")
+vetor = f.read().split(",")
+for i in range(len(vetor)):
+    vetor[i] = float(vetor[i])
+ndvetor = np.asarray(vetor)
+ndvetor = ndvetor.reshape(Nx, Nx)
+
+fig = plt.figure(figsize=(8, 6), dpi=128)#, facecolor='k', edgecolor='k')
+gs = gridspec.GridSpec(4, 1)#, width_ratios=[1, 1])
+#ax = fig.add_subplot(111)
+ax = plt.subplot(gs[:3, :])
+ax.imshow(ndvetor, cmap=maps, interpolation='gaussian', origin='lower')
+ax = plt.subplot(gs[3:, :])
+ax.set_xlabel('time (h)', fontweight='bold')
+ax.set_ylabel(r'flux (L$\star$)', fontweight='bold')
+ax.grid(which='major', c=color, alpha=.6, lw=.6)
+ax.grid(which='minor', c=color, alpha=.3, lw=.3)
+ax.set_facecolor('k')
+for sp in ('left','bottom', 'right', 'top'):
+	ax.spines[sp].set_color(color)
+ax.xaxis.label.set_color(color)
+ax.yaxis.label.set_color(color)
+ax.tick_params(axis='both', which='both', colors=color)
+fig.tight_layout()			
+fig.savefig('noise.png', dpi=128, facecolor='k')
+plt.close('all')
+ 
